540aa1b8bd42ad85f2c6a5e7017da4312d4dbdfb
Michael Stephanou <mmstephanou@gmail.com>
2022-05-17 09:23:56 +0100

    heroku deployment

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { port } from './config/environment.js';
 import router from './config/router.js';
 import { connectToDb } from './db/helpers.js';
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api', router);
 
